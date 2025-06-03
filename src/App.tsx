dbb13aaa913e14bda110ae4ed779f58a21d9c1d4
Piotr Durniat <piotr@durniat.com>
2025-06-03 18:04:56 +0200

    router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import { Button } from "@/components/ui/button"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { RootLayout } from "./components/layout/RootLayout"
+import { Page1 } from "./pages/Page1"
+import { Page2 } from "./pages/Page2"
+import { Page3 } from "./pages/Page3"
+import { Page4 } from "./pages/Page4"
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<RootLayout />}>
+          <Route path="/" element={<Page1 />} />
+          <Route path="/page2" element={<Page2 />} />
+          <Route path="/page3" element={<Page3 />} />
+          <Route path="/page4" element={<Page4 />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/layout/RootLayout.tsx
@@ -0,0 +1,42 @@
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList } from "@/components/ui/navigation-menu"
+import { Link, Outlet } from "react-router-dom"
+
+export function RootLayout() {
+  return (
+    <div className="min-h-svh flex flex-col">
+      <header className="fixed top-0 left-0 right-0 border-b bg-background z-50">
+        <div className="container mx-auto py-4">
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuLink asChild>
+                  <Link to="/">Page 1</Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink asChild>
+                  <Link to="/page2">Page 2</Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink asChild>
+                  <Link to="/page3">Page 3</Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink asChild>
+                  <Link to="/page4">Page 4</Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+      </header>
+      <main className="flex-1 container mx-auto py-8 mt-[60px] px-4">
+        <div className="bg-background rounded-lg border p-4">
+          <Outlet />
+        </div>
+      </main>
+    </div>
+  )
+} 
